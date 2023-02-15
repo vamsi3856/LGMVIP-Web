c35369e768ae8e14d6faf62fa8127f8d049c90b7
MUPPURI VENKATESWARA VAMSI KUMAR <103525054+vamsi3856@users.noreply.github.com>
2023-02-15 19:05:34 +0530

    Add files via upload

---
+++ Task-2/src/components/cards.js
@@ -0,0 +1,35 @@
+import React from "react";
+import kittenHeader from '../images/load.mp4';
+
+const Users = ({ loading, users }) => {
+  const mainstyle = {
+    display: "flex"
+  };
+  return loading ? (
+    <div id="main" align="center">
+      <video
+        src={ kittenHeader }
+        alt="Loaading.."
+        className="loader"
+        autoPlay
+      />
+    </div>
+  ) : (
+    <div id="main" style={mainstyle}>
+      {users.map((user) => (
+        <div className="profile_cont" key={user.id}>
+          {console.log(user)}
+
+          <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+          <h1 className="name">
+            {user.first_name} {user.last_name}
+          </h1>
+          <p className="email">{user.email}</p>
+          <p>User ID: {user.id}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
